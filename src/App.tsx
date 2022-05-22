33c16d82b48a3154e278b958756e5773546a3014
Abdul Aziz Safudin <me@ziz.ninja>
2022-05-22 19:58:36 +0800

    chore(lint): run linter

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,10 @@ const App = () => {
           Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla nec.
         </p>
         <a
-        href="#"
-        className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50 sm:w-auto"
-        onClick={() => setCount(count + 1)}
-      >
+          href="#"
+          className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50 sm:w-auto"
+          onClick={() => setCount(count + 1)}
+        >
           Counter: {count}
         </a>
       </div>
