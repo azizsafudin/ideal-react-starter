e150a67d466d4b75897bfe6737f5342fad3b30e2
Abdul Aziz Safudin <me@ziz.ninja>
2022-05-22 17:59:55 +0800

    test: add vitest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    'jest/globals': true
   },
   extends: ['plugin:react/recommended', 'plugin:react/jsx-runtime', 'standard'],
   parser: '@typescript-eslint/parser',
